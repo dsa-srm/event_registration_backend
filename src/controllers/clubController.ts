1945c65127b038db1cab5ff7dbe51e9a96fced69
Ankit kumar <ankit.kumar.info22@gmail.com>
2023-09-06 22:00:09 +0530

    deleted js files

--- src/controllers/clubController.ts
+++ src/controllers/clubController.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express';
 
 //write your code here
-// if you code exceeds the max length of 60 lines, then write your code in parts inside service and export them here.
+// if you code exceeds the max length of 50 lines, then write your code in parts inside service and export them here.
 
 export const addClubDetails = async (req:Request, res:Response) => {
+    console.log("inside post")
 
 res.send('Hello, TypeScript Backend!');
 }
 export const getClubDetails = async (req:Request, res:Response) => {
+    console.log("inside get")
     res.send('Hello, TypeScript Backend!');
 
 
