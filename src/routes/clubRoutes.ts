9334e3b5ad3b304197932ceb94ffb1f48cba2111
Ankit kumar <ankit.kumar.info22@gmail.com>
2023-09-13 22:45:31 +0530

    update

--- src/routes/clubRoutes.ts
+++ src/routes/clubRoutes.ts
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.post('/clubDetail',addClubDetails);  //add club details
 router.get('/clubDetail',getClubDetails);   //get club details
-router.get('/clubDetail',getClubDetails);   //get club details
 // router.delete('/clubDetail',getClubDetails);   //get club details
 // router.put('/clubDetail',getClubDetails);   //get club details
 
