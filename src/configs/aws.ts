524757ba69127023d762b4cdb16dc251235a5558
Ankit kumar <ankit.kumar.info22@gmail.com>
2023-09-14 21:41:15 +0530

    rds update

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -4,7 +4,6 @@ dotenv.config();
 import pgPromise from "pg-promise";
 const fs = require('fs');
 const pgp = pgPromise();
-// const pemFile = require('../certificates/global-bundle.pem');
 const dbConfig = {
   host: process.env.DBHOST,
   port: 5432,
@@ -12,8 +11,7 @@ const dbConfig = {
   user: process.env.USER,
   password: process.env.PASSWORD,
   ssl: {
-    // ca: fs.readFileSync(pemFile),
-    rejectUnauthorized: true, // this was the problem
+    rejectUnauthorized: true, 
   },
 };
 const db = pgp(dbConfig);
